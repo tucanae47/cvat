df44b12086a24ccf6aaad4bf321456bcb1a30349
tucanae47 <cmsvalenzuela@gmail.com>
2019-05-03 11:24:26 -0500

    updated openvino install script to support openvino 2019

--- components/openvino/install.sh
+++ components/openvino/install.sh
@@ -12,7 +12,7 @@ if [[ `lscpu | grep -o "GenuineIntel"` != "GenuineIntel" ]]; then
 fi
 
 if [[ `lscpu | grep -o "sse4" | head -1` != "sse4" ]] && [[ `lscpu | grep -o "avx2" | head -1` != "avx2" ]]; then
-    echo "OpenVINO expects your CPU to support SSE4 or AVX2 instructions"
+    echo "You Intel CPU should support sse4 or avx2 instruction if you want use OpenVINO"
     exit 1
 fi
 
@@ -23,21 +23,10 @@ tar -xzf `ls | grep "openvino_toolkit"`
 cd `ls -d */ | grep "openvino_toolkit"`
 
 apt-get update && apt-get install -y sudo cpio && \
- if [ -f "install_cv_sdk_dependencies.sh" ]; then ./install_cv_sdk_dependencies.sh; \
- else ./install_openvino_dependencies.sh; fi && SUDO_FORCE_REMOVE=yes apt-get remove -y sudo
+ ./install_openvino_dependencies.sh && SUDO_FORCE_REMOVE=yes apt-get remove -y sudo
 
-cat ../eula.cfg >> silent.cfg
-./install.sh -s silent.cfg
-
-cd /tmp/components && rm openvino -r
-
-if [ -f "/opt/intel/computer_vision_sdk/bin/setupvars.sh" ]; then
-    echo "source /opt/intel/computer_vision_sdk/bin/setupvars.sh" >> ${HOME}/.bashrc;
-    echo -e '\nexport IE_PLUGINS_PATH=${IE_PLUGINS_PATH}' >> /opt/intel/computer_vision_sdk/bin/setupvars.sh;
-else
-    echo "source /opt/intel/openvino/bin/setupvars.sh" >> ${HOME}/.bashrc;
-    echo -e '\nexport IE_PLUGINS_PATH=${IE_PLUGINS_PATH}' >> /opt/intel/openvino/bin/setupvars.sh;
-fi
+apt-get update && apt-get install -y sudo cpio && \
+SUDO_FORCE_REMOVE=yes apt-get remove -y sudo
 
 
 
@@ -46,5 +35,5 @@ cat ../eula.cfg >> silent.cfg
 
 cd /tmp/components && rm openvino -r
 
-echo "source /opt/intel/computer_vision_sdk/bin/setupvars.sh" >> ${HOME}/.bashrc
-echo -e '\nexport IE_PLUGINS_PATH=${IE_PLUGINS_PATH}' >> /opt/intel/computer_vision_sdk/bin/setupvars.sh
+echo "source /opt/intel/openvino/bin/setupvars.sh" >> ${HOME}/.bashrc
+echo -e '\nexport IE_PLUGINS_PATH=${IE_PLUGINS_PATH}' >> /opt/intel/openvino/bin/setupvars.sh
